087a7a756a3350f79eba2e4926d61185d8f83455
Bent Cardan <bent@nothingsatisfies.com>
2015-02-06 15:35:16 -0700

    tidy

--- test/nanomsg.sockopt.js
+++ test/nanomsg.sockopt.js
@@ -11,14 +11,19 @@ describe('nanomsg.sockopt', function() {
   rep.connect('tcp://127.0.0.1:44449')
 
   it('should set tcp nodelay', function (done){
+
     req.setsockopt('NN_TCP','NN_TCP_NODELAY',1).should.equal(0)
+
     done()
   })
 
   it('should get tcp nodelay setting', function (done){
+
     var get = req.getsockopt('NN_TCP','NN_TCP_NODELAY')
+
     get[0].should.equal(0)
     get[1].should.equal(1)
+    
     done()
   })
 })
