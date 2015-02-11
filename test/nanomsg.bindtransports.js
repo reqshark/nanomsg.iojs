a6a9ad9582856ac892f2e66cbf7e998354bf2db3
Bent Cardan <bent@nothingsatisfies.com>
2015-02-10 19:58:29 -0500

    rename bind test file: use for protocol tests

---
+++ test/nanomsg.bindtransports.js
@@ -0,0 +1,19 @@
+var nano    = require('..')
+var should  = require('should')
+var semver  = require('semver')
+
+describe('nanomsg.transports', function() {
+
+  it('should bind, connect, send and receive over tcp', function(done){
+    done()
+  })
+
+  it('should bind, connect, send and receive over inproc', function(done){
+    done()
+  })
+
+  it('should bind, connect, send and receive over ipc', function(done){
+    done()
+  })
+
+})
